0b5a7b61217c19fe7dfce4e855b34acb4c7c1a9b
Poliak <ppoliak@luxoft.com>
2022-12-09 01:58:42 +0200

    Created transport Inheritance

--- src/main/java/com/homework/inherit/Inheritance.java
+++ src/main/java/com/homework/inherit/Inheritance.java
@@ -1,209 +1,317 @@
 package com.homework.inherit;
 
+/**
+ * Abstract class Vehicle with Getters and Setters, Constructors, @Override toString() and fields: 
+ * <li>String name;
+ * <li>double distance;
+ * <li>double fuel;
+ * <li>boolean run;
+ * <p>
+ * Abstract classes Auto, Fly, Ship, Liner extends Abstract class Vehicle 
+ * <p>
+ * Abstract class SailLiner extends abstract class Liner
+ * <p>
+ * Abstract class AeroPlane extends abstract class Fly
+ * <p>
+ * Classes Car, Bus, Truck extends abstract class Auto 
+ * <p>
+ * Classes TugBoat, Tanker extends abstract class Ship 
+ * <p>
+ * Class PassengersSailLiner extends abstract class Liner
+ * <p>
+ * Class Fighter extends abstract class Fly
+ * <p>
+ * Classes TransportAeroPlane and PassengersAeroPlane extends abstract class AeroPlane
+ * */
 class Inheritance {
 	public static void main(String[] args) {
-		VehicleManager manager = new VehicleManager();
-		Object create = manager.create();
-		System.out.println(create.toString());
-		
-//		Transport a = new Transport();
-//		Factory<Bus> busFactory = new BusFactory();
-//		Factory<Auto> autoFactory = new AutoFactory();
-//		Vehicle vehicle = busFactory.create();
-//		Auto dog = autoFactory.create();
-//		System.out.println(dog.toString());
 
+		VehicleFactory carFactory = new CarFactory();
+		Vehicle vehicle = carFactory.create();
+		vehicle.run();
+		System.out.println(vehicle);
 	}
-}
-
- interface Transport {
-	void run();
-	String info();
 }
 
 interface Factory<T> {
 	T create();
 }
 
-class VehicleManager implements Transport, Factory<Vehicle>{
-	
-	private Auto auto;
+abstract interface VehicleFactory extends Factory<Vehicle> {
+}
 
-	@Override
-	public Auto create() {
-		this.auto = new Auto();
-		return auto;
-	}
-
-	@Override
-	public void run() {
-		boolean run = auto.isRun();
-		auto.s !run ? true : false;
-		
-	}
-
-	@Override
-	public String info() {
-		return auto.info();
+class CarFactory implements VehicleFactory {
+	public Car create() {
+		return new Car();
 	}
 }
 
-
-
-abstract interface VehicleFactory extends Factory<Vehicle> {	
-	public default Vehicle create() {
-		return new Vehicle();
-	}
-}
-
-class AutoFactory implements Factory<Auto> {
-	public Auto create() {
-		return new Auto();
-	}
-}
-
-class BusFactory implements Factory<Bus> {	
-	public Bus create() {
-		return new Bus();
-	}
-}
-
-class TruckFactory implements Factory<Truck> {	
+class TruckFactory implements Factory<Truck> {
 	public Truck create() {
 		return new Truck();
 	}
 }
 
-class Auto extends Vehicle {
-	public String info() {
-		return this.toString();
+abstract class Fly extends Vehicle {
+	public Fly(String name) {
+		super(name);
 	}
-	public Auto(int wheels) {
-		super(wheels);
-	}
-	public Auto() {
+
+	String info() {
+		return name;
+	};
+}
+
+class PassengerAeroPlane extends AeroPlane {
+	
+	public PassengerAeroPlane(String name) {
+		super(name);
 	}
 }
 
-class Bus extends Vehicle {
-	public Bus(int wheels) {
-		super(wheels);
+class TransportAeroPlane extends AeroPlane {
+	
+	public TransportAeroPlane(String name) {
+		super(name);
 	}
-	public Bus() {
+}
+
+abstract class AeroPlane extends Fly {
+	
+	public AeroPlane(String name) {
+		super(name);
+	}
+}
+class Fighter extends Fly {
+	
+	public Fighter(String name) {
+		super(name);
+	}
+}
+
+abstract class Ship extends Vehicle {
+	public Ship(String name) {
+		super(name);
+	}
+
+	String info() {
+		return name;
+	};
+}
+
+abstract class Liner extends Vehicle {
+
+	public Liner(String name) {
+		super(name);
+	}
+}
+
+abstract class SailLiner extends Liner {
+
+	public String info() {
+		return name;
+	}
+
+	public SailLiner(String name) {
+		super(name);
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Sail Liner [name=%s, fuel=%f, distance=%f, run=%s]", 
+				name, fuel, distance, run);
+	}
+}
+
+class PassengersSailLiner extends SailLiner {
+
+	public PassengersSailLiner(String name) {
+		super(name);
+	}
+	public String info() {
+		return name;
 	}
 	@Override
 	public String toString() {
 		return String.format(
-				"Bus [wheels=%d, weight=%f, speed=%f, run=%b]", 
-				 getWheels(), getWeight(), getSpeed(), isRun());
+				"Passengers Sail Liner [name=%s, fuel=%f, distance=%f, run=%s]", 
+				name, fuel, distance, run);
+	}
+}
+
+class TugBoat extends Ship {
+
+	public String info() {
+		return name;
+	}
+
+	public TugBoat(String name) {
+		super(name);
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Tugboat [name=%s, fuel=%f, distance=%f, run=%s]", 
+				 name, fuel, distance, run);
+	}
+}
+
+class Tanker extends Ship {
+
+	public double fuel() {
+		return super.fuel;
+	}
+	public double distance() {
+		return super.distance;
+	}
+	public String info() {
+		return name;
+	}
+
+	public Tanker(String name) {
+		super(name);
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Tanker [name=%s, fuel=%f, distance=%f, run=%s]", 
+				 name, fuel, distance, run);
+	}
+}
+
+abstract class Auto extends Vehicle {
+	protected int wheels;
+
+	public Auto(String name) {
+		super(name);
 	}
 	
-}
-
-class Truck extends Vehicle {
-	public Truck(int wheels) {
-		super(wheels);
+	public Auto() {
+		super("Auto");
+		this.wheels = 4;
 	}
 
-	public Truck() {
-	}
-}
-
-class Vehicle implements Transport {
-	private int wheels;
-	private double weight;
-	private double speed;
-	private boolean run;
-
-	public Vehicle(int wheels) {
-		this.wheels = wheels;
-	}
-
-	public int getWheels() {
+	public int wheels() {
 		return wheels;
 	}
 
-	public void setWheels(int wheels) {
+	public void wheels(int wheels) {
 		this.wheels = wheels;
 	}
+}
 
-	public double getWeight() {
-		return weight;
+class Car extends Auto {
+
+	public String info() {
+		return name;
 	}
 
-	public void setWeight(double weight) {
-		this.weight = weight;
-	}
-
-	public double getSpeed() {
-		return speed;
-	}
-
-	public void setSpeed(double speed) {
-		this.speed = speed;
+	public Car(String name) {
+		super(name);
 	}
 	
-	public boolean isRun() {
-		return run;
-	}
-
-	public void run(boolean run) {
-		this.run = run;
+	public Car() {
+		super("Car");
 	}
 
 	@Override
 	public String toString() {
 		return String.format(
-				"Vehicle [wheels=%d, weight=%f, speed=%f, run=%b]", 
-				wheels, weight, speed, run);
+				"Car [name=%s, wheels=%d, fuel=%f, distance=%f, run=%s]", 
+				name, wheels, fuel, distance,run);
+	}
+}
+
+class Bus extends Auto {
+
+	public String info() {
+		return name;
 	}
 
-	public Vehicle() {
-		this.wheels = 4;
+	public Bus(String name) {
+		super(name);
+	}
+
+	public Bus() {
+		super("Bus");
+		}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Bus [name=%s, wheels=%d, fuel=%f, distance=%f, run=%s]", 
+				wheels, name, fuel, distance, run);
+	}
+}
+
+class Truck extends Auto {
+
+	public double fuel() {
+		return super.fuel;
+	}
+
+	public String info() {
+		return name;
+	}
+
+	public Truck(String name) {
+		super(name);
+	}
+
+	public Truck() {
+		super("Truck");
+}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Truck [name=%s, wheels=%d, fuel=%f, distance=%f, run=%s]", 
+				wheels, name, fuel, distance, run);
+	}
+}
+
+abstract class Vehicle {
+
+	protected String name;
+	protected double distance;
+	protected double fuel;
+	protected boolean run;
+
+	public Vehicle(String name) {
+		this.name = name;
 	}
 	
-	class Ship implements Transport {
-		private double weight;
-		private double speed;
-		private boolean run;
-		public double getWeight() {
-			return weight;
-		}
-		public void setWeight(double weight) {
-			this.weight = weight;
-		}
-		public double getSpeed() {
-			return speed;
-		}
-		public void setSpeed(double speed) {
-			this.speed = speed;
-		}
-		public boolean isRun() {
-			return run;
-		}
-		public void run(boolean run) {
-			this.run = run;
-		}
-		@Override
-		public void run() {
-			run = !run ? true : false;
-		}
-		@Override
-		public String info() {
-			// TODO Auto-generated method stub
-			return null;
-		}	
+	public Vehicle() {
+		this.name = "Vehicle";
 	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
+	void run() {
+		run = !run ? true : false;
+	};
 
 	@Override
-	public String info() {
-		// TODO Auto-generated method stub
-		return null;
+	public String toString() {
+			return String.format(
+				"Vehicle %s: fuel=%f, distance=%f work=%b", 
+				name, fuel, distance, run);
+	}
+
+	public double fuel() {
+		return fuel;
+	}
+
+	public void fuel(double fuel) {
+		this.fuel = fuel;
+	}
+
+	public double distance() {
+		return distance;
+	}
+
+	public void distance(double distance) {
+		this.distance = distance;
 	}
 }
